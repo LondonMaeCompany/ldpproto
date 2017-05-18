4bc333a30d778002603c20643e43deed92f587a7
Art Stevenson <art.stevenson@londonmae.com>
2017-05-18 10:25:25 -0400

    Menu Changes
    Editied the Navbar menu

---
+++ navbar.php
@@ -0,0 +1,67 @@
+<header class="header header_mod-a">
+
+  <div class="header-main banner2">
+    <div class="container">
+      <div class="row navbar-banner">
+        <div class="col-xs-12"><a href="home.html" class="logo space"><img src="assets/media/general/logo_type-a.png" width="200px" height="auto" alt="Logo" class="logo__img img-responsive"/></a>
+          <div class="header-nav">
+            <div class="navbar-trigger visible-xs">
+              <button type="button" data-toggle="collapse" data-target="#navbar-collapse-1" class="navbar-toggle"><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
+            </div><a href="home.html" class="btn-primary header-main__btn btn btn-sm btn-effect">Get In Touch</a>
+            <nav class="navbar yamm">
+              <div id="navbar-collapse-1" class="navbar-collapse collapse">
+                <ul class="nav navbar-nav">
+                  <li class="dropdown"><a href="home.html">Home</a>
+                    <ul role="menu" class="dropdown-menu">
+                      <li><a href="home.html">LockDown Protection</a></li>
+                      <li><a href="home-2.html">LockDown Mobile</a></li>
+                    </ul>
+                  </li>
+                  <li><a href="about.html">Who We Are</a></li>
+                  <li class="dropdown"><a href="services.html">What We Offer</a>
+                    <ul role="menu" class="dropdown-menu">
+                      <li><a href="services.html">Services</a></li>
+                      <li><a href="service-details.html">Pricing</a></li>
+                    </ul>
+                  </li>
+
+                  <li class="dropdown"><a href="home.html">Software & Support</a>
+                    <ul role="menu" class="dropdown-menu">
+                      <li><a href="pricing-faqs.html">Software Downloads</a></li>
+                      <li><a href="pricing-faqs.html">How to Videos</a></li>
+                      <li><a href="typography.html">Customer Support</a></li>
+                    </ul>
+                  </li>
+                  <li class="dropdown"><a href="post-1.html">See More</a>
+                    <ul role="menu" class="dropdown-menu">
+                      <li><a href="post-1.html">Our Work</a></li>
+                      <li><a href="post-2.html">Proven Protection</a></li>
+                      <li><a href="post-single.html">Customer Reviews</a></li>
+                    </ul>
+                  </li>
+                  <li><a href="contact.html">Contact</a></li>
+                    <li class="social-net__item"><a href="https://www.facebook.com/lockdownprotectioninc/" class="social-net__link"><i class="icon fa fa-facebook"></i></a></li>
+                </ul>
+              </div>
+              <div class="modal-search">
+                <div class="container">
+                  <div class="row">
+                    <div class="col-sm-8 col-sm-offset-2 col-xs-10 col-xs-offset-1">
+                      <form class="form-search-global">
+                        <input type="text" placeholder="Type to search" class="form-search-global__input"/>
+                        <button class="form-search-global__btn"><i class="icon fa fa-search"></i></button>
+                        <div class="form-search-global__note">Begin typing your search above and press return to search.</div>
+                      </form>
+                    </div>
+                  </div>
+                </div>
+                <button type="button" class="modal-search__close close"><i class="fa fa-times"></i></button>
+              </div>
+            </nav>
+            <!-- end nav-->
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</header>
