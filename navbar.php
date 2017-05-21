cf7784ddaeaaf9dba730699d27ad5f1c50e52dbf
Art Stevenson <art.stevenson@londonmae.com>
2017-05-21 10:21:06 -0400

    Update 3.2
    Updated the navigation bar links and Removed Color changing gear

--- navbar.php
+++ navbar.php
@@ -1,13 +1,34 @@
-<header class="header header_mod-a">
-
-  <div class="header-main banner2">
+<?php
+<header class="header">
+  <div class="header-top clearfix">
     <div class="container">
-      <div class="row navbar-banner">
-        <div class="col-xs-12"><a href="home.html" class="logo space"><img src="assets/media/general/logo_type-a.png" width="200px" height="auto" alt="Logo" class="logo__img img-responsive"/></a>
+      <div class="row">
+        <div class="col-xs-12">
+          <ul class="social-net list-inline">
+            <li class="social-net__item"><a href="twitter.com" class="social-net__link"><i class="icon fa fa-twitter"></i></a></li>
+            <li class="social-net__item"><a href="facebook.com" class="social-net__link"><i class="icon fa fa-facebook"></i></a></li>
+            <li class="social-net__item"><a href="linkedin.com" class="social-net__link"><i class="icon fa fa-linkedin"></i></a></li>
+            <li class="social-net__item"><a href="vimeo.com" class="social-net__link"><i class="icon fa fa-vimeo"></i></a></li>
+            <li class="social-net__item"><a href="rss.com" class="social-net__link"><i class="icon fa fa-rss"></i></a></li>
+            <li class="social-net__item"><a href="plus.google.com" class="social-net__link"><i class="icon fa fa-google-plus"></i></a></li>
+          </ul>
+          <!-- end social-list-->
+          <div class="header-contact">
+            <div class="header-contact__item">Call  :<span class="header-contact__info"> 404 200 3124</span></div>
+            <div class="header-contact__item">Email  :<a href="mailto:LOCKHART@LOCKDOWNPROTECTION.COM" class="header-contact__info"> LOCKHART@LOCKDOWNPROTECTION.COM</a></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="header-main">
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-12"><a href="home.html" class="logo"><img src="assets/media/general/logo_type-c.png" alt="Logo" class="logo__img img-responsive"/></a>
           <div class="header-nav">
             <div class="navbar-trigger visible-xs">
               <button type="button" data-toggle="collapse" data-target="#navbar-collapse-1" class="navbar-toggle"><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
-            </div><a href="home.html" class="btn-primary header-main__btn btn btn-sm btn-effect">Get In Touch</a>
+            </div><a href="home.html" class="btn-second header-main__btn btn btn-sm btn-effect">Get In Touch</a>
             <nav class="navbar yamm">
               <div id="navbar-collapse-1" class="navbar-collapse collapse">
                 <ul class="nav navbar-nav">
@@ -21,13 +42,13 @@
                   <li class="dropdown"><a href="services.html">What We Offer</a>
                     <ul role="menu" class="dropdown-menu">
                       <li><a href="services.html">Services</a></li>
-                      <li><a href="service-details.html">Pricing</a></li>
+                      <li><a href="Pricing-faqs.html">Pricing</a></li>
                     </ul>
                   </li>
 
                   <li class="dropdown"><a href="home.html">Software & Support</a>
                     <ul role="menu" class="dropdown-menu">
-                      <li><a href="pricing-faqs.html">Software Downloads</a></li>
+                      <li><a href="support.html">Software Downloads</a></li>
                       <li><a href="pricing-faqs.html">How to Videos</a></li>
                       <li><a href="typography.html">Customer Support</a></li>
                     </ul>
@@ -39,25 +60,11 @@
                       <li><a href="post-single.html">Customer Reviews</a></li>
                     </ul>
                   </li>
-                  <li><a href="contact.html">Contact</a></li>
-                    <li class="social-net__item"><a href="https://www.facebook.com/lockdownprotectioninc/" class="social-net__link"><i class="icon fa fa-facebook"></i></a></li>
+
+
                 </ul>
               </div>
-              <div class="modal-search">
-                <div class="container">
-                  <div class="row">
-                    <div class="col-sm-8 col-sm-offset-2 col-xs-10 col-xs-offset-1">
-                      <form class="form-search-global">
-                        <input type="text" placeholder="Type to search" class="form-search-global__input"/>
-                        <button class="form-search-global__btn"><i class="icon fa fa-search"></i></button>
-                        <div class="form-search-global__note">Begin typing your search above and press return to search.</div>
-                      </form>
-                    </div>
-                  </div>
-                </div>
-                <button type="button" class="modal-search__close close"><i class="fa fa-times"></i></button>
-              </div>
-            </nav>
+             </nav>
             <!-- end nav-->
           </div>
         </div>
@@ -65,3 +72,4 @@
     </div>
   </div>
 </header>
+ ?>
