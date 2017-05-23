a45064e34822ddf31be350613787bc76f7943d3a
Tyler Roberts <Tyler Roberts>
2017-05-23 17:04:12 -0400

    blog routing

--- blog/scripts/controllers/posts.js
+++ blog/scripts/controllers/posts.js
@@ -5,10 +5,54 @@
         .module('app')
         .controller('PostsController', PostsController);
 
-    PostsController.$inject = ['$scope'];
-    function PostsController($scope) {
+    PostsController.$inject = ['$scope', '$routeParams'];
+    function PostsController($scope, $routeParams) {
         var vm = this;
-        vm.Hello = "Hello";
+
+        console.log($routeParams.id)
+        vm.post = undefined;
+        
+        var Posts = [
+            {
+                'id': 0,
+                'entryDate': '15 jun 2016',
+                'entryUser': 'April',
+                'comments': 10,
+                'entryTitle': 'Lorem ipsum dolor sit amet consectetur elit sed',
+                'entryContent': "Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex commodo consequat. Duis aute irure dolor reprehend erit in voluptate velit ese cillum dolore eu fugiat nulla pariatur.Excepteur sint occaecat cupidat at non proident sunt in culpa offica deserunt mollit anim id est quia laborum.",
+            },{
+                'id': 1,
+                'entryDate': '15 jun 2017',
+                'entryUser': 'Cory',
+                'comments': 20,
+                'entryTitle': 'Lorem ipsum dolor sit amet consectetur elit sed',
+                'entryContent': "Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex commodo consequat. Duis aute irure dolor reprehend erit in voluptate velit ese cillum dolore eu fugiat nulla pariatur.Excepteur sint occaecat cupidat at non proident sunt in culpa offica deserunt mollit anim id est quia laborum.",
+            },{
+                'id': 2,
+                'entryDate': '15 jan 2016',
+                'entryUser': 'Tyler',
+                'comments': 25,
+                'entryTitle': 'Lorem ipsum dolor sit amet consectetur elit sed',
+                'entryContent': "Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex commodo consequat. Duis aute irure dolor reprehend erit in voluptate velit ese cillum dolore eu fugiat nulla pariatur.Excepteur sint occaecat cupidat at non proident sunt in culpa offica deserunt mollit anim id est quia laborum.",
+            },
+        ];
+        
+
+        function init () {
+            if ($routeParams.id === undefined) {
+                // location = 404 page
+            }else{
+                vm.post = Posts[$routeParams.id]
+            }
+        };
+
+        init();
+
+
+
+
+
+
 
     }
 })();
